7d01aa996455f844e88c1989eb5dd81e92bbd810
m-qm <mireiaquerol@gmail.com>
2020-04-13 17:43:37 +0200

    useEffect fetching

--- src/index.js
+++ src/index.js
@@ -1,16 +1,74 @@
-import React, { useState } from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 
-import { BrowserRouter as Router } from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 import CharacterList from './CharacterList';
 
-import dummyData from './dummy-data';
+import endpoint from './endpoint';
 
 import './styles.scss';
 
+// const initialState = {
+//   result: null,
+//   loading: true,
+//   error: null,
+// };
+
+// const fetchReducer = (state, action) => {
+//   if (action.type === 'LOADING') {
+//     return {
+//       result: null,
+//       loading: true,
+//       error: null,
+//     };
+//   }
+
+//   if (action.type === 'RESPONSE_COMPLETE') {
+//     return {
+//       result: action.payload.response,
+//       loading: false,
+//       error: null,
+//     };
+//   }
+//   if (action.type === 'ERROR') {
+//     return {
+//       result: null,
+//       loading: false,
+//       error: action.payload.error,
+//     };
+//   }
+//   return state;
+// };
+
+const useFetch = url => {
+  const [response, setResponse] = useState ([]);
+  const [loading, setLoading] = useState (true);
+  const [error, setError] = useState (null);
+
+  useEffect (() => {
+    setLoading (true);
+    setResponse (null);
+    setError (null);
+
+    fetch (endpoint + '/characters')
+      .then (response => response.json ())
+      .then (response => {
+        setLoading (false);
+        setResponse (response);
+      })
+      .catch (error => {
+        setLoading (false);
+        setError (error);
+      });
+  }, []);
+  return [response, loading, error];
+};
+
 const Application = () => {
-  const [characters, setCharacters] = useState(dummyData);
+  const [response, loading, error] = useFetch (endpoint + '/characters');
+
+  const characters = (response && response.characters) || [];
 
   return (
     <div className="Application">
@@ -19,18 +77,19 @@ const Application = () => {
       </header>
       <main>
         <section className="sidebar">
-          <CharacterList characters={characters} />
+          {loading ? <p>Loading</p> : <CharacterList characters={characters} />}
         </section>
+        {error && <p className="error">{error}</p>}
       </main>
     </div>
   );
 };
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById ('root');
 
-ReactDOM.render(
+ReactDOM.render (
   <Router>
     <Application />
   </Router>,
-  rootElement,
+  rootElement
 );
